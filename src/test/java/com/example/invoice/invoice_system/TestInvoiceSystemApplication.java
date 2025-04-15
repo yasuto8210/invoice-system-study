dec72e91e86e14591aded1396e70db8cac6e394d
yasuto <yasuto8210@icloud.com>
2025-04-15 11:54:18 +0900

    Resolve initial build issue

--- src/test/java/com/example/invoice/invoice_system/TestInvoiceSystemApplication.java
+++ src/test/java/com/example/invoice/invoice_system/TestInvoiceSystemApplication.java
@@ -1,6 +1,5 @@
 package com.example.invoice.invoice_system;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class TestInvoiceSystemApplication {
