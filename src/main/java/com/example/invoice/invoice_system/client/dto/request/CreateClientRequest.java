c723699c1188e7e7d166abb65f0fc746c3cd4d64
yasuto <yasuto8210@icloud.com>
2025-04-24 04:28:14 +0900

    #1 add schema

--- src/main/java/com/example/invoice/invoice_system/client/dto/request/CreateClientRequest.java
+++ src/main/java/com/example/invoice/invoice_system/client/dto/request/CreateClientRequest.java
@@ -1,13 +1,19 @@
 package com.example.invoice.invoice_system.client.dto.request;
 
 import com.example.invoice.invoice_system.client.domain.Client;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class CreateClientRequest {
 
+    @Schema(description = "name", example = "tom")
     private String name;
+    @Schema(description = "E-mail", example = "<EMAIL>")
     private String email;
+    @Schema(description = "phone number", example = "000-0000-0000")
     private String phone;
+    @Schema(description = "address", example = "address")
     private String address;
+    @Schema(description = "note", example = "note")
     private String notes;
 
     public CreateClientRequest() {
--- src/main/java/com/example/invoice/invoice_system/client/dto/response/ClientResponse.java
+++ src/main/java/com/example/invoice/invoice_system/client/dto/response/ClientResponse.java
@@ -1,9 +1,11 @@
 package com.example.invoice.invoice_system.client.dto.response;
 
-public record ClientResponse(Long id,
-                             String name,
-                             String email,
-                             String phone,
-                             String address,
-                             String notes) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ClientResponse(@Schema(description = "ID", example = "1") Long id,
+                             @Schema(description = "name", example = "tom") String name,
+                             @Schema(description = "E-mail", example = "test@test.com") String email,
+                             @Schema(description = "phone number", example = "000-0000-0000") String phone,
+                             @Schema(description = "address", example = "address") String address,
+                             @Schema(description = "note", example = "note") String notes) {
 }
